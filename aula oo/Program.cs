8f1efe092448ffce175d29399a98135acc045631
jeisoncolisse <jeisoncolisse31@gmail.com>
2026-04-09 21:02:33 -0300

    Add files via upload

---
+++ aula oo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Informações do time
+        string nome = "SER Caxias do Sul";
+        string fundacao = "10/04/1935";
+        string tecnico = "Marcelo Cabo";
+
+        List<string> titulos = new List<string>
+        {
+            "Campeonato Gaúcho 2000",
+            "Campeonato Brasileiro Série C 2002"
+        };
+
+        // Exibindo dados
+        Console.WriteLine("=== Dados do Time ===");
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Fundação: " + fundacao);
+        Console.WriteLine("Técnico: " + tecnico);
+        Console.WriteLine("Títulos:");
+
+        foreach (string titulo in titulos)
+        {
+            Console.WriteLine("- " + titulo);
+        }
+    }
+}
