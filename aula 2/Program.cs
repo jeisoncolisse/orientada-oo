102df51124d237d95dfe4ae64997089f908f9229
jeisoncolisse <jeisoncolisse31@gmail.com>
2026-04-10 21:00:01 -0300

    Add files via upload

---
+++ aula 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Produto
+{
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+    public int Estoque { get; set; } 
+
+    public Produto(string nome, decimal preco, int estoque)
+    {
+        Nome = nome;
+        Preco = preco;
+        Estoque = estoque;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        Produto produto = new Produto("Notebook", 3500.00m, 10);
+
+        Console.WriteLine($"Nome: {produto.Nome}");
+        Console.WriteLine($"Preço: {produto.Preco}");
+        Console.WriteLine($"Estoque: {produto.Estoque}");
+    }
+}
